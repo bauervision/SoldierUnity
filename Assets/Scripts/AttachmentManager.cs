552a1576583e538b7a3ae4c93364f27ea9fa460b
McB <mike.bauer@octo.us>
2024-12-10 14:20:24 -0500

    wip attachments part 1

--- Assets/Scripts/AttachmentManager.cs
+++ Assets/Scripts/AttachmentManager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using VRSoldier;
 
 public class AttachmentManager : MonoBehaviour
 {
     public static AttachmentManager instance { get; private set; }
+    public GameObject uiPrefab, packObjectUI, helmetObjectUI, weaponObjectUI; // Assign the UI prefab in the Inspector 
+    HighlightableObject _packObject, _helmetObject, _weaponObject;
+    public Transform contentParent; // Assign the Scroll View Content in the Inspector
+    public TextMeshProUGUI totalText, totalAttachedText;
+    public Slider slider;
     public UnityEvent clearPacks = new();
     public UnityEvent clearHelmets = new();
     public UnityEvent clearWeapons = new();
 
+    int totalAttached = 0;
     List<HighlightableObject> highlightableObjects = new();
 
     private void Awake() { instance = this; }
@@ -20,7 +29,39 @@ public class AttachmentManager : MonoBehaviour
         HandleSingleAttachments(highlightableObject.transform.GetComponent<LoadItem>().weapon.itemType);
 
         highlightableObject.AttachModel();
-        highlightableObjects.Add(highlightableObject);
+        // now let's find out if we need to collect the attachments in a list
+        ItemType itemType = highlightableObject.transform.GetComponent<LoadItem>().weapon.itemType;
+        AttachTypes(itemType, highlightableObject);
+
+    }
+
+    /// <summary>
+    /// There can only 1 Pack attached to the soldier, this method ensures this
+    /// </summary>
+    public void HandleSingleAttachments(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            case ItemType.WEAPON: clearWeapons.Invoke(); break;
+            case ItemType.PACK: clearPacks.Invoke(); break;
+            case ItemType.HELMET: clearHelmets.Invoke(); break;
+        }
+
+
+    }
+
+    void AttachTypes(ItemType itemType, HighlightableObject highlightableObject)
+    {
+        switch (itemType)
+        {
+            case ItemType.WEAPON: _weaponObject = highlightableObject; break;
+            case ItemType.PACK: _packObject = highlightableObject; break;
+            case ItemType.HELMET: _helmetObject = highlightableObject; break;
+            // case ItemType.MASK: _weaponObject = highlightableObject; break;
+            // case ItemType.PISTOL: _weaponObject = highlightableObject; break;
+            // case ItemType.VEST: _weaponObject = highlightableObject; break;
+            default: highlightableObjects.Add(highlightableObject); break;
+        }
     }
 
     public void RemoveAttachment(int index)
@@ -29,28 +70,180 @@ public class AttachmentManager : MonoBehaviour
         highlightableObjects.Remove(highlightableObjects[index]);
     }
 
-    /// <summary>
-    /// There can only 1 Pack attached to the soldier, this method ensures this
-    /// </summary>
-    public void HandleSingleAttachments(ItemType itemType)
+
+
+    float RoundToTwoDecimalPlaces(float value) { return Mathf.Round(value * 100f) / 100f; }
+
+    string HandleWeightTextUpdate(float weight)
     {
-        // check to see if we already have a a single item attached which can only have 1
-        foreach (HighlightableObject item in highlightableObjects)
+        slider.value = weight;
+        return HighlightManager.instance.totalWeight + "lbs";
+    }
+
+    public void AddattachmentToUI()
+    {
+        // now we  need to handle the 4 types of attachments, first find out which type this is
+        HandleTypeAttachmentUI();
+
+        // handle attachment
+        AddAttachment(HighlightManager.instance.currentLoadItem.transform.GetComponent<HighlightableObject>());
+
+
+        // Play a random click sound using AudioManager 
+        if (AudioManager.Instance != null) { AudioManager.Instance.PlaySound(HighlightManager.instance.currentLoadItem.weapon.itemType); }
+    }
+
+
+    float _lastWeaponWeight = 0, _lastPackWeight = 0, _lastHelmetWeight = 0;
+    void HandleTypeAttachmentUI()
+    {
+        switch (HighlightManager.instance.currentLoadItem.weapon.itemType)
         {
-            // we found a pack already attached
-            if ((item.GetComponent<LoadItem>().weapon.itemType == ItemType.PACK) && (itemType == ItemType.PACK))
-                clearPacks.Invoke();
+            case ItemType.WEAPON:
+                {
+                    // if this item was not yet added
+                    if (!weaponObjectUI.activeInHierarchy)
+                    {
+                        totalAttached++;
+                        totalAttachedText.text = "Total Attached: " + totalAttached;
+                        weaponObjectUI.SetActive(true);
+                    }
+                    // set the text
+                    weaponObjectUI.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = HighlightManager.instance.currentLoadItem.weapon.itemName + " : " + HighlightManager.instance.currentLoadItem.weapon.itemWeight;
+                    // handle the weight
+                    float newWeight = HighlightManager.instance.currentLoadItem.weapon.itemWeight;
+                    float oldWeight = _lastWeaponWeight;
+                    UpdateTotalWeight(newWeight, oldWeight);
+                    _lastWeaponWeight = newWeight;
+                    break;
+                }
+            case ItemType.HELMET:
+                {
+                    if (!helmetObjectUI.activeInHierarchy)
+                    {
+                        totalAttached++;
+                        totalAttachedText.text = "Total Attached: " + totalAttached;
+                        helmetObjectUI.SetActive(true);
+                    }
+                    // set the text
+                    helmetObjectUI.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = HighlightManager.instance.currentLoadItem.weapon.itemName + " : " + HighlightManager.instance.currentLoadItem.weapon.itemWeight;
+                    // handle the weight
+                    float newWeight = HighlightManager.instance.currentLoadItem.weapon.itemWeight;
+                    float oldWeight = _lastHelmetWeight;
+                    UpdateTotalWeight(newWeight, oldWeight);
+                    _lastHelmetWeight = newWeight;
+                    break;
+                }
+            case ItemType.PACK:
+                {
+                    if (!packObjectUI.activeInHierarchy)
+                    {
+                        totalAttached++;
+                        totalAttachedText.text = "Total Attached: " + totalAttached;
+                        packObjectUI.SetActive(true);
+                    }
+                    // set the text
+                    packObjectUI.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = HighlightManager.instance.currentLoadItem.weapon.itemName + " : " + HighlightManager.instance.currentLoadItem.weapon.itemWeight;
+                    // handle the weight
+                    float newWeight = HighlightManager.instance.currentLoadItem.weapon.itemWeight;
+                    float oldWeight = _lastPackWeight;
+                    UpdateTotalWeight(newWeight, oldWeight);
+                    _lastPackWeight = newWeight;
+                    break;
+                }
+            default:// basic attachments
+                {
+                    totalAttached++;
+                    totalAttachedText.text = "Total Attached: " + totalAttached;
 
-            // we found a pack already attached
-            if ((item.GetComponent<LoadItem>().weapon.itemType == ItemType.WEAPON) && (itemType == ItemType.WEAPON))
-                clearWeapons.Invoke();
+                    GameObject instantiatedUI = Instantiate(uiPrefab, contentParent);
+                    // set all the text values
+                    instantiatedUI.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = HighlightManager.instance.currentLoadItem.weapon.itemName + " : " + HighlightManager.instance.currentLoadItem.weapon.itemWeight;
+                    instantiatedUI.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = HighlightManager.instance.currentLoadItem.weapon.itemType.ToString();
+                    // handle the button
+                    Button removeButton = instantiatedUI.transform.GetChild(1).GetComponent<Button>();
+                    float weight = HighlightManager.instance.currentLoadItem.weapon.itemWeight;
+                    removeButton.onClick.AddListener(() => HandleRemoval(instantiatedUI, weight));
 
-            // we found a pack already attached
-            if ((item.GetComponent<LoadItem>().weapon.itemType == ItemType.HELMET) && (itemType == ItemType.HELMET))
-                clearHelmets.Invoke();
-
+                    UpdateTotalWeight(weight, 0);
+                    break;
+                }
 
         }
+    }
+
+    void UpdateTotalWeight(float newWeight, float oldWeight)
+    {
+        float updatedWeight = HighlightManager.instance.totalWeight + newWeight - oldWeight;
+        HighlightManager.instance.totalWeight = RoundToTwoDecimalPlaces(Mathf.Clamp(updatedWeight, 0f, 300));
+        totalText.text = HandleWeightTextUpdate(HighlightManager.instance.totalWeight);
 
     }
+
+    public void RemovePack()
+    {
+        totalAttached--;
+        totalAttachedText.text = "Total Attached: " + totalAttached;
+        float newweight = HighlightManager.instance.totalWeight - _lastPackWeight;
+        HighlightManager.instance.totalWeight = RoundToTwoDecimalPlaces(Mathf.Clamp(newweight, 0f, 300f));
+        // Check if totalWeight is very close to zero, and set it to zero if so 
+        if (Mathf.Abs(HighlightManager.instance.totalWeight) < 0.001f) { HighlightManager.instance.totalWeight = 0f; }
+        totalText.text = HandleWeightTextUpdate(HighlightManager.instance.totalWeight);
+
+        _packObject.transform.GetComponent<HighlightableObject>().DetachModel();
+        _packObject = null;
+        packObjectUI.SetActive(false);
+        if (AudioManager.Instance != null) { AudioManager.Instance.PlayRemove(); }
+
+    }
+
+    public void RemoveHelmet()
+    {
+        totalAttached--;
+        totalAttachedText.text = "Total Attached: " + totalAttached;
+        float newweight = HighlightManager.instance.totalWeight - _lastPackWeight;
+        HighlightManager.instance.totalWeight = RoundToTwoDecimalPlaces(Mathf.Clamp(newweight, 0f, 300f));
+        // Check if totalWeight is very close to zero, and set it to zero if so 
+        if (Mathf.Abs(HighlightManager.instance.totalWeight) < 0.001f) { HighlightManager.instance.totalWeight = 0f; }
+        totalText.text = HandleWeightTextUpdate(HighlightManager.instance.totalWeight);
+
+        _helmetObject.transform.GetComponent<HighlightableObject>().DetachModel();
+        _helmetObject = null;
+        helmetObjectUI.SetActive(false);
+        if (AudioManager.Instance != null) { AudioManager.Instance.PlayRemove(); }
+    }
+
+    public void RemoveWeapon()
+    {
+        totalAttached--;
+        totalAttachedText.text = "Total Attached: " + totalAttached;
+        float newweight = HighlightManager.instance.totalWeight - _lastPackWeight;
+        HighlightManager.instance.totalWeight = RoundToTwoDecimalPlaces(Mathf.Clamp(newweight, 0f, 300f));
+        // Check if totalWeight is very close to zero, and set it to zero if so 
+        if (Mathf.Abs(HighlightManager.instance.totalWeight) < 0.001f) { HighlightManager.instance.totalWeight = 0f; }
+        totalText.text = HandleWeightTextUpdate(HighlightManager.instance.totalWeight);
+
+        _weaponObject.transform.GetComponent<HighlightableObject>().DetachModel();
+        _weaponObject = null;
+        weaponObjectUI.SetActive(false);
+        if (AudioManager.Instance != null) { AudioManager.Instance.PlayRemove(); }
+    }
+
+    public void HandleRemoval(GameObject removeButton, float weight)
+    {
+        totalAttached--;
+        totalAttachedText.text = "Total Attached: " + totalAttached;
+        float newweight = HighlightManager.instance.totalWeight - weight;
+        HighlightManager.instance.totalWeight = RoundToTwoDecimalPlaces(Mathf.Clamp(newweight, 0f, 300f));
+        // Check if totalWeight is very close to zero, and set it to zero if so 
+        if (Mathf.Abs(HighlightManager.instance.totalWeight) < 0.001f) { HighlightManager.instance.totalWeight = 0f; }
+        totalText.text = HandleWeightTextUpdate(HighlightManager.instance.totalWeight);
+
+        RemoveAttachment(removeButton.transform.GetComponentIndex());
+
+        Destroy(removeButton);
+
+        if (AudioManager.Instance != null) { AudioManager.Instance.PlayRemove(); }
+    }
+
 }
--- Assets/Scripts/HighlightManager.cs
+++ Assets/Scripts/HighlightManager.cs
@@ -7,26 +7,28 @@ namespace VRSoldier
 {
     public class HighlightManager : MonoBehaviour
     {
+        public static HighlightManager instance;
         public float MAXWEIGHT = 300;
         public MouseAndGamepadAimController mouseAndGamepadAimController;
-        public TextMeshProUGUI itemText, weightText, weaponText, totalText, totalAttachedText, maxWeightText;
+        public TextMeshProUGUI itemText, weightText, weaponText, maxWeightText;
         public Image highlightIcon;
-        public Slider slider;
+
         public Color highlightIconColor, originalIconColor;
-        public GameObject uiPrefab; // Assign the UI prefab in the Inspector 
-        public Transform contentParent; // Assign the Scroll View Content in the Inspector
+
         private HighlightableObject currentHighlightedObject;
 
-        LoadItem currentLoadItem;
+        public LoadItem currentLoadItem;
 
-        float totalWeight = 0;
-        int totalAttached = 0;
+        public float totalWeight = 0;
+
         bool showCursor = false;
         bool canAttach = true;
 
 
 
         HighlightableObject _tempObject;
+
+        private void Awake() { instance = this; }
 
         private void Start()
         {
@@ -51,7 +53,7 @@ namespace VRSoldier
                     if (totalWeight <= MAXWEIGHT)
                     {
                         canAttach = true;
-                        AddattachmentToUI();
+                        AttachmentManager.instance.AddattachmentToUI();
                     }
                     else
                     {
@@ -134,59 +136,5 @@ namespace VRSoldier
             mouseAndGamepadAimController.enabled = !state;
         }
 
-        void AddattachmentToUI()
-        {
-            totalAttached++;
-            totalAttachedText.text = "Total Attached: " + totalAttached;
-            // Instantiate the UI prefab and set its parent to the Scroll View Content 
-            if (uiPrefab != null && contentParent != null)
-            {
-
-                float newweight = totalWeight + currentLoadItem.weapon.itemWeight;
-                totalWeight = RoundToTwoDecimalPlaces(Mathf.Clamp(newweight, 0f, 300));
-                totalText.text = HandleWeightTextUpdate(totalWeight);
-
-                GameObject instantiatedUI = Instantiate(uiPrefab, contentParent);
-                // set all the text values
-                instantiatedUI.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = currentLoadItem.weapon.itemName + " : " + currentLoadItem.weapon.itemWeight;
-                instantiatedUI.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = currentLoadItem.weapon.itemType.ToString();
-                // handle the button
-                Button removeButton = instantiatedUI.transform.GetChild(1).GetComponent<Button>();
-                float weight = currentLoadItem.weapon.itemWeight;
-                removeButton.onClick.AddListener(() => HandleRemoval(instantiatedUI, weight));
-            }
-
-            // handle attachment
-            AttachmentManager.instance.AddAttachment(currentLoadItem.transform.GetComponent<HighlightableObject>());
-
-
-            // Play a random click sound using AudioManager 
-            if (AudioManager.Instance != null) { AudioManager.Instance.PlaySound(currentLoadItem.weapon.itemType); }
-        }
-
-        void HandleRemoval(GameObject removeButton, float weight)
-        {
-            totalAttached--;
-            totalAttachedText.text = "Total Attached: " + totalAttached;
-            float newweight = totalWeight - weight;
-            totalWeight = RoundToTwoDecimalPlaces(Mathf.Clamp(newweight, 0f, 300f));
-            // Check if totalWeight is very close to zero, and set it to zero if so 
-            if (Mathf.Abs(totalWeight) < 0.001f) { totalWeight = 0f; }
-            totalText.text = HandleWeightTextUpdate(totalWeight);
-
-            AttachmentManager.instance.RemoveAttachment(removeButton.transform.GetComponentIndex());
-
-            Destroy(removeButton);
-
-            if (AudioManager.Instance != null) { AudioManager.Instance.PlayRemove(); }
-        }
-
-        string HandleWeightTextUpdate(float weight)
-        {
-            slider.value = weight;
-            return totalWeight + "lbs";
-        }
-
-        float RoundToTwoDecimalPlaces(float value) { return Mathf.Round(value * 100f) / 100f; }
     }
 }
